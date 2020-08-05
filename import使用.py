8a4361fe78e15b4fe3fa2f3c2564031c00abb715
huawuque18 <33893148+huawuque18@users.noreply.github.com>
2020-08-05 11:27:20 +0800

    #11:21时间commit

--- import使用.py
+++ import使用.py
@@ -1,7 +1,7 @@
-
 import sys
 import time
 import os
+
 print('================Python import mode==========================')
 print ('命令行参数为:')
 for i in sys.argv:
@@ -10,7 +10,7 @@ print ('\n python 路径为',sys.path)
 print('\n')
 print(os.getcwd())
 print(os.path)
-print(os.stat('main.py'))
+print(os.stat('基本语法.py'))
 print('提示操作系统名',os.name)
 print(os.sep,"间隔符")
 
@@ -18,26 +18,4 @@ print(os.extsep)
 
 print(repr(os.linesep))
 print(time.time())
-print(os.path.getatime("main.py"))
-
-
-'''
-if True:
-    print ("true")
-else:
-    print ("False")
-'''
-
-#num1=input("shuru:")
-#print(num1)
-num1=10
-num2=12
-#total = num1 + num2
-#print (total)
-
-#word= input("请输入把：")
-#print(word[0:2],word[2])
-
-
-#11:04时间commit
-#11:21时间commit
+print(os.path.getatime("基本语法.py"))
---
+++ 基本语法.py
@@ -0,0 +1,14 @@
+
+
+
+'''
+if True:
+    print ("true")
+else:
+    print ("False")
+'''
+
+#num1=input("shuru:")
+#print(num1)
+num1=10
+num2=12
