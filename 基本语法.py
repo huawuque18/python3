932b357b87c3e8892fcbdc42dcc529609502148d
huawuque18 <33893148+huawuque18@users.noreply.github.com>
2020-08-05 11:59:50 +0800

    提交了关于print的用法

--- 基本语法.py
+++ 基本语法.py
@@ -1,14 +1,21 @@
+#多行注释可以用多个 # 号，还有 ''' 和 """：
+'''
+第三注释
+'''
 
-
-
+"""
+第五注释
+"""
+print("Hello, Python!")
 '''
 if True:
     print ("true")
-else:
+else:1
     print ("False")
 '''
 
-#num1=input("shuru:")
-#print(num1)
-num1=10
+'''接收输入'''
+num1=input("请输入你的变量信息:")
+#打印输入的变量，并且
+print("你输入的文字是:",(num1),sep='')
 num2=12
